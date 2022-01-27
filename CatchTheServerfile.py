b98fe2903436e4b9f2e79278d1d54bab15c07376
sjrjq <98275343+sjrjq@users.noreply.github.com>
2022-01-27 23:20:55 +0900

    Add files via upload

--- CatchTheServerfile.py
+++ CatchTheServerfile.py
@@ -8,18 +8,33 @@ Copyright 2022. sjrjq all rights reserved.
 
 from PyQt5.QtWidgets import *
 import sys
-import os
+import re
+import datetime
+
+
 
 
 VirusDB = [
-    'kro.kr',
-    'codns',
-    'kro'
+    'codns:DOMAIN',
+    'Software\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run:SetStartup',
+    'SOFTWARE\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\Run:SetStartup',
+    'kro.kr:DOMAIN',
+    'HARDWARE\\Description\\System\\SystemProductName:grabPCinfo',
+    'HARDWARE\\Description\\System\\SystemProductName:grabPCinfo',
+    '\\x13[ENTER]:njrat_payload_src',
+    '\\x1bprocesshacker\\x0bdnSpy:njrat_payload_src2',
+    '\\x07\\x04\\x02\\x02\\x12\\x80\\x91\\x12\\x80\\x91/\\x07\\x13\\x0e\\x1d\\x0e\\x1d\\x0e:njratASM'
 ]
+
 
 vdb = []
 
 filelist = [None]
+
+STATUS_CODE = None
+
+for pattern in VirusDB:
+    vdb.append(pattern)
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -29,7 +44,7 @@ class MyWindow(QWidget):
     
     def setupUI(self):
         
-        self.setGeometry(500,500,500,500)
+        self.setGeometry(500,500,500,100)
         self.setWindowTitle("CTS [Open Source]")
  
         self.OnOpenDocument_Button = QPushButton("파일 열기")
@@ -39,7 +54,7 @@ class MyWindow(QWidget):
         self.Button2.clicked.connect(self.letsgo)
 
         self.label = QLabel()
-        self.label.setText("검사 결과 :")
+        self.label.setText("...")
 
         self.label2 = QLabel()
         self.label2.setText("Copyright 2022. sjrjq all rights reserved.")
@@ -47,7 +62,6 @@ class MyWindow(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.OnOpenDocument_Button)
         layout.addWidget(self.Button2)
-        layout.addWidget(self.Button3)
         layout.addWidget(self.label)
         layout.addWidget(self.label2)
 
@@ -61,30 +75,27 @@ class MyWindow(QWidget):
             QMessageBox.about(self, "Warning!", "Please select file.")
 
     def letsgo(self):
-        file = open(filelist[0], "rb")
 
+        now = datetime.datetime.now()
+        datem = str(now.strftime('%Y-%m-%d %H%M%S'))+'.log'
+
+        file = open(filelist[0], "rb")
+        file2 = open("log/"+datem, "w")
+        FP = open("A.txt", "w")
         rded = file.readlines()
 
         x = str(rded).replace('\\x00','')
-
-
-        for pattern in VirusDB:
-            a = x.find(pattern)
-            vdb.append(a)
-
-        for i in range(len(vdb)):
-            if vdb[i] != -1:
-                self.label.setText("발견됨 : "+VirusDB[1])
-                os.remove(filelist[0])
-                vdb.clear()
-                break
-            else:
-                self.label.setText("발견되지 않음")
-                vdb.clear()
-                break
-            
         
+        FP.write(x)
 
+        for i in vdb:
+            px = i.split(':')
+            if x.find(px[0]) == -1:
+                file2.write('[not found]  ' + i + '\n')
+            else:
+                file2.write('[found]  ' + i + '\n')
+        self.label.setText("스캔이 완료되었습니다. " + datem + "를 확인하세요.")
+                
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
